54c32618d40531dda55dd9726151ce970a1e77df
Aamez <aamez@pawzeeble.com>
2023-08-31 00:24:55 +0530

    analytics integration

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "@styles/globals.css";
 
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "Promptopia",
@@ -19,6 +20,7 @@ const RootLayout = ({ children }) => (
         <main className='app'>
           <Nav />
           {children}
+          <Analytics />
         </main>
       </Provider>
     </body>
